6ca80933dab775d3a7a73c48d75217bfae88a7fc
Toby Retallick <toby.retallick@gmail.com>
2014-06-10 09:34:55 +0100

    FileValues parses JSON correctly

--- lib/file_values.rb
+++ lib/file_values.rb
@@ -1,19 +1,25 @@
 class FileValues
 
+	FILETYPES = {
+		video: ['mp4', 'avi'],
+		song:['mp3','wav'],
+		document: ['pdf', 'doc', 'docx', 'odt'],
+		text: ['txt'],
+		binary: ['bin']
+	}
+
 	def initialize(request)
 		@request = request
 	end
 
-	#def filenames
-	#	@request['files'].map { |file| file['name'] }
-	#end
-#
-	#def filesizes
-	#	@request['files'].map { |file| '%.1f' % (file['size'] / 1000000) }
-	#end
+	def parse
+		@request['files'].map do |file|
+			[file['name'], file['extension'], file['size']]
+		end
+	end
 
 	def all_files
-		@request['files'].map  do |file| 
+		@request['files'].map do |file| 
 			["#{ file['name'] }.#{ file['extension'] }", "#{ '%.1f' % (file['size'] / 1000000) }"]
 		end
 	end
--- spec/file_values_spec.rb
+++ spec/file_values_spec.rb
@@ -7,6 +7,14 @@ describe 'Calculating file values from JSON' do
 	let(:request) { WorkshareApi.new('toby.retallick@gmail.com', ENV['WORKSHARE_PASS'] ).request_files }
 	let(:values) { FileValues.new(request) }
 
+	context 'parsing files' do
+		it 'returns the name, file type and size of each file' do
+			expect(values.parse[0]).to eq(["01 Dancehall State Of Mind", "mp3", 6311732 ])
+			expect(values.all_files.length).to eq(9)
+		end
+
+	end
+
 	context 'listing files' do
 
 		it 'returns a string for each file with formatted name and file size' do
@@ -14,19 +22,6 @@ describe 'Calculating file values from JSON' do
 			expect(values.all_files.length).to eq(9)
 			expect(values.all_files[0]).to eq(["01 Dancehall State Of Mind.mp3", "6.0"])
 	
-		end
-
-	end
-
-	context 'calculating file weights' do
-
-		it 'can calculate the frequency of file types' do
-		end
-
-		it 'can calculate the gravity for each file' do
-		end
-
-		it 'can calculate the total gravity for all files' do
 		end
 
 	end
